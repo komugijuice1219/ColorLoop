13664d2b2d27fc87bdc46d5cd0de82c8bf2ee379
tao <taozhedan1219@gmail.com>
2024-11-20 11:46:54 +0900

    Initial Commit

---
+++ ColorLoop/ColorLoopApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorLoopApp.swift
+//  ColorLoop
+//
+//  Created by たお1219 on 2024/11/20.
+//
+
+import SwiftUI
+
+@main
+struct ColorLoopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColorLoop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ColorLoop
+//
+//  Created by たお1219 on 2024/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
